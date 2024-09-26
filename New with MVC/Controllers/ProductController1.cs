b9ed76965d8b7733eb9f7f9139400cd07c803709
Vishal Prajapati <vishalp951295@gmail.com>
2024-09-26 08:28:10 +0530

    Add project files.

---
+++ New with MVC/Controllers/ProductController1.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace New_with_MVC.Controllers
+{
+    public class ProductController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult Productview()
+        {
+            return View("Index1");
+        }
+    }
+}
